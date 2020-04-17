ad1d5b99f99f3c957855c028713c74a5fdadb68a
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-04-17 13:25:58 +0800

    first covid-19 app code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import * as stateAbbr from "datasets-us-states-abbr";
+import Chart from "./Chart/Chart";
+
+/*import logo from './logo.svg';
+import './App.css';*/
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // eslint-disable-next-line no-unused-vars
+    const [activeQuery, setActiveQuery] = useState("NY");
+    // eslint-disable-next-line no-unused-vars
+    const [stateData, setStateData] = useState({});
+
+    useEffect(() => {
+        const getData = async () => {
+            const res = await fetch(`https://covidtracking.com/api/states?state=${activeQuery}`);
+            const data = await res.json();
+            setStateData(data);
+        };
+        getData().then();
+    }, [activeQuery]);
+    return (
+        <div className="App">
+            <div style={{width: "600px", textAlign: "center"}}>
+                <h1>COVID-19 Test Tracker</h1>
+                <form>
+                    <label>
+                        Select a state:
+                        <select value={activeQuery} onChange={e => setActiveQuery(e.target.value)}>
+                            {Object.values(stateAbbr).map(abbr => {
+                                return (
+                                    <option value={abbr} key={abbr}>{abbr}</option>
+                                )
+                            })}
+                        </select>
+                    </label>
+                </form>
+                <Chart data={stateData} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
